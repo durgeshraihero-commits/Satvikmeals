44af261c41b3b4f9c4c3f793aae3f4094d2ecde6
Durgesh Rai <durgeshraihero@gmail.com>
2025-12-23 23:35:07 +0530

    Add admin dashboard home page

--- app/admin/page.js
+++ app/admin/page.js
@@ -2,15 +2,15 @@ import Link from "next/link";
 
 export default function AdminHome() {
   return (
-    <div style={{ padding: 40 }}>
-      <h1>Admin Dashboard</h1>
+    <div style={{ padding: 20 }}>
+      <h1>👨‍💼 Admin Panel</h1>
 
-      <ul style={{ marginTop: 20 }}>
+      <ul>
+        <li><Link href="/admin/menu">Manage Menu</Link></li>
+        <li><Link href="/admin/addons">Manage Add-ons</Link></li>
         <li><Link href="/admin/orders">Orders</Link></li>
         <li><Link href="/admin/users">Users</Link></li>
         <li><Link href="/admin/payments">Payments</Link></li>
-        <li><Link href="/admin/menu">Menu</Link></li>
-        <li><Link href="/admin/addons">Addons</Link></li>
         <li><Link href="/admin/complaints">Complaints</Link></li>
       </ul>
     </div>
